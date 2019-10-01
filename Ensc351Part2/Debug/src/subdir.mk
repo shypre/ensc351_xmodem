56ca75cd24b849d4bfcad557470c92906ee60c40
osboxes.org <osboxes@osboxes>
2019-10-01 12:26:59 -0700

    add original fine for comparison

--- Ensc351Part2/Debug/src/subdir.mk
+++ Ensc351Part2/Debug/src/subdir.mk
@@ -32,7 +32,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -I"/mnt/hgfs/VMsf/workspace-cpp-OxygenSeptember/Ensc351" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -I"/mnt/hgfs/VMsf/workspace-cpp-OxygenSeptember/ensc351_proj/Ensc351" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
