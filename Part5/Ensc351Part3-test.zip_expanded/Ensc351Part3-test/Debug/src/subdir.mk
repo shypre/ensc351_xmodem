cbcd5826e4ff11378e5949d8236884b818a7641a
osboxes.org <osboxes@osboxes>
2019-11-19 16:40:19 -0800

    fix part 1/2

---
+++ Part5/Ensc351Part3-test.zip_expanded/Ensc351Part3-test/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Ensc351Part3-test.cpp 
+
+OBJS += \
+./src/Ensc351Part3-test.o 
+
+CPP_DEPS += \
+./src/Ensc351Part3-test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++1y -I"/home/osboxes/Desktop/ensc351_proj/Part5/Ensc351part2-Linux-Soln-Tues.zip_expanded/Ensc351Part2SolnLib" -I"/home/osboxes/Desktop/ensc351_proj/Part5/Ensc351" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
